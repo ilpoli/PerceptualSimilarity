6dc8fbce91305ac3da8d0ec884a0d95136612b64
Ilya Polishchuk <ilya.polishchuk@akvelon.com>
2022-07-15 23:27:06 +0300

    Update lpips.py

--- lpips/lpips.py
+++ lpips/lpips.py
@@ -80,6 +80,8 @@ class LPIPS(nn.Module):
             net_type = pn.squeezenet
             self.chns = [64,128,256,384,384,512,512]
         self.L = len(self.chns)
+        self.weights = [1.0] * self.L
+        self.weights = [0.0,0.0,0.0,0.0,1.0]
 
         self.net = net_type(pretrained=not self.pnet_rand, requires_grad=self.pnet_tune)
 
@@ -136,7 +138,7 @@ class LPIPS(nn.Module):
 
         val = 0
         for l in range(self.L):
-            val += res[l]
+            val += res[l] * self.weights[l]
         
         if(retPerLayer):
             return (val, res)
